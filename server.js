8efdc4bf3d64996940b20c2402a8fcb507f1e102
Sungjun <hongsi140626@gmail.com>
2019-06-24 11:39:44 -0400

    commit for port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 // const router = require('./routers/router.js');
 const bodyParser = require('body-parser');
 // const mongoose = require("mongoose");
